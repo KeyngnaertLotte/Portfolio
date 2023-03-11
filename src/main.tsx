033073366aa7af968ed8375d14cadbe7d3f2d5a2
KeyngnaertLotte <lotte.keyngnaert@student.howest.be>
2023-03-11 20:48:30 +0100

    feat: homepage done

--- src/main.tsx
+++ src/main.tsx
@@ -6,16 +6,17 @@ import AboutMe from './routes/AboutMe'
 import Cv from './routes/Cv'
 import Home from './routes/Home'
 import Portfolio from './routes/Portfolio'
+import './assets/tailwind.css'
 
 const router = createBrowserRouter([
   { path: '/', element: <Home /> },
-  { path: '/aboutme', element: <AboutMe/> },
-  { path: '/cv', element: <Cv/> },
-  { path: '/portfolio', element: <Portfolio/>}
+  { path: '/aboutme', element: <AboutMe /> },
+  { path: '/cv', element: <Cv /> },
+  { path: '/portfolio', element: <Portfolio /> },
 ])
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+      <RouterProvider router={router} />
   </React.StrictMode>,
 )
--- src/routes/AboutMe.tsx
+++ src/routes/AboutMe.tsx
@@ -1,7 +1,12 @@
+import AppNavigation from "../components/AppNavigation";
+
 export default function AboutMe() {
   return (
-    <div>
-      <h1>AboutMe</h1>
-    </div>
+    <main className="flex h-screen justify-end bg-black bg-[url('../assets/img/BgHome.jpg')] bg-no-repeat align-middle">
+      <div className="flex flex-col justify-center align-middle p-8 w-2/4">
+        <h1 className='text-white'>Lotte Keyngnaert</h1>
+        <AppNavigation/>
+      </div>
+    </main>
   );
 }
