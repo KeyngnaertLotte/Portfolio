4fa65c94d12cb3526807901f2dc2f5a48b4ebf22
KeyngnaertLotte <lotte.keyngnaert@student.howest.be>
2023-04-27 16:21:15 +0200

    feat: taalkennis web done

--- src/components/CV/AppComputervaardigheden.tsx
+++ src/components/CV/AppComputervaardigheden.tsx
@@ -1,72 +1,13 @@
-import { useState } from 'react'
 import { Languages, Code2, Paintbrush, LayoutGrid, Layout } from 'lucide-react'
 
 export default () => {
   return (
-    // <div className="flex h-full w-11/12 flex-col items-center text-white">
-    //   <h2 className=" m-8 text-center font-heading text-4xl font-medium">
-    //     Computervaardigheden & Taalkennis
-    //   </h2>
-    //   <div className="flex h-auto w-3/5 flex-wrap justify-center">
-    //     <div className="m-5 h-52 w-2/5 rounded p-5 shadow shadow-accent">
-    //       <h3 className="col-span-2 mb-5 flex flex-row items-center gap-2 font-heading text-2xl font-medium">
-    //         <Languages className="stroke-white" />
-    //         Taalkennis
-    //       </h3>
-    //       <div className='grid grid-cols-2'>
-    //         <p className="col-span-full  font-body">
-    //           Nederlands (moedertaal)
-    //         </p>
-    //         <p className="col-start-1 row-start-3 mb-4 font-body font-light">
-    //           vloeiend
-    //         </p>
-    //         <p className="col-start-1 row-start-4 font-body">Engels</p>
-    //         <p className="col-start-1 row-start-5 font-body font-light">
-    //           vloeiend
-    //         </p>
-    //         <p className="col-start-2 row-start-4 font-body">Frans</p>
-    //         <p className="col-start-2 row-start-5 font-body font-light">
-    //           basis
-    //         </p>
-    //       </div>
-    //     </div>
-    //     <div className="m-5 h-52 w-2/5 rounded p-5 shadow shadow-accent">
-    //       <h3 className="mb-5 flex  flex-row items-center gap-2 font-heading text-2xl font-medium">
-    //         <Code2 className="stroke-white" />
-    //         Codeertalen
-    //       </h3>
-    //       <p className="font-body">
-    //         Html, css, Javascript, Typescript, Python, C#, Typescript, SQL en
-    //         beetje C++
-    //       </p>
-    //     </div>
-    //     <div className="m-5 h-52 w-2/5 rounded p-5 shadow shadow-accent">
-    //       <h3 className="mb-5 flex  flex-row items-center gap-2 font-heading text-2xl font-medium">
-    //         <Paintbrush className="stroke-white" />
-    //         Adobe Apps
-    //       </h3>
-    //       <p className="font-body">
-    //         Photoshop, Illustrator, Indesign, After Effects, Xd, Premiere Pro
-    //       </p>
-    //     </div>
-    //     <div className="m-5 h-52 w-2/5 rounded p-5 shadow shadow-accent">
-    //       <h3 className="mb-5 flex  flex-row items-center gap-2 font-heading text-2xl font-medium">
-    //         <LayoutGrid className="stroke-white" />
-    //         Andere Apps
-    //       </h3>
-    //       <p className="font-body">
-    //         Visual Studio Code, Visual Studio, Atom, MySQL, Atom, Word, Excel,
-    //         PowerPoint
-    //       </p>
-    //     </div>
-    //   </div>
-    // </div>
     
-    <div className="flex h-full flex-col items-center text-white mt-10 tablet:h-4/5 ">
-      <h2 className=" text-center font-heading text-[1.7rem] font-medium leading-8 tablet:text-4xl">
+    <div className="flex h-full flex-col items-center text-white mt-10 tablet:h-4/5  web:w-11/12 ">
+      <h2 className=" text-center font-heading text-[1.7rem] font-medium leading-8 tablet:text-4xl web:m-8 ">
         Computervaardigheden & Taalkennis
       </h2>
-      <div className="flex h-full w-full flex-col justify-center items-center tablet:flex-wrap tablet:flex-row tablet:h-fit tablet:mt-24">
+      <div className="flex h-full w-full flex-col justify-center items-center tablet:flex-wrap tablet:flex-row tablet:h-fit tablet:mt-24  web:h-auto web:w-3/5 web:mt-0">
         <div className="h-fit w-full p-2 border-b-2 border-accent tablet:w-2/5 tablet:h-44 tablet:p-5 tablet:m-5 tablet:shadow tablet:shadow-accent tablet:border-0">
           <h3 className="col-span-2 mb-1 flex flex-row items-center gap-2 font-heading text-2xl font-medium">
             <Languages className="stroke-white" />
--- src/routes/Cv.tsx
+++ src/routes/Cv.tsx
@@ -36,20 +36,6 @@ export default function Cv() {
     
   
   return (
-    // <main className="flex h-screen w-full flex-col items-center bg-black p-4">
-    //   <div className="flex w-9/12 flex-row justify-around">
-    //     <h1 className="w-11/12 pb-5 font-heading text-4xl font-bold text-white">
-    //       Lotte Keyngnaert
-    //     </h1>
-    //     <AppNavigation />
-    //   </div>
-    //     {active === 0 ? <AppOpleiding/> : active === 1 ? <AppWerkervaring/> : <AppComputervaardigheden/>}
-    //   <div className='flex flex-row gap-4'>
-    //     <button className={`w-24 h-1 bg-white rounded ${active === 0 ? `opacity-1` : 'opacity-50'}`} onClick={() => setActive(0)}/>
-    //     <button className={`w-24 h-1 bg-white rounded ${active === 1 ? `opacity-1` : 'opacity-50'}`} onClick={() => setActive(1)}/>
-    //     <button className={`w-24 h-1 bg-white rounded ${active === 2 ? `opacity-1` : 'opacity-50'}`} onClick={() => setActive(2)}/>
-    //   </div>
-    // </main>
     <main className="h-screen w-screen flex flex-row">
         
     <div className={`bg-black z-20 overflow-hidden flex flex-col ${showNav ? 'translate-x-0 absolute top-0 left-0 h-screen w-screen  p-4' : 'translate-x-full none h-0 w-0  p-0'}`}>
