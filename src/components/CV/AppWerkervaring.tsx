5e8429635591fc50691ef04fd2a88e731593d6ad
KeyngnaertLotte <lotte.keyngnaert@student.howest.be>
2023-04-27 15:36:34 +0200

    feat: werkervaring web done

--- src/components/CV/AppWerkervaring.tsx
+++ src/components/CV/AppWerkervaring.tsx
@@ -38,6 +38,7 @@ export default () => {
     },
   ])
   const isTablet = useMediaQuery({ query: '(min-width:560px)' })
+  const isDesktop = useMediaQuery({ query: '(min-width:840px)' })
   const [showInfo, setShowInfo] = useState(0)
 
   const showingInfo = (index: number) => {
@@ -127,16 +128,17 @@ export default () => {
     //     <p className='font-body italic'>Bij diversen</p>
     //   </div>
     // </div>
-    <div className="grid grid-cols-1 grid-rows-mobile text-white h-full tablet:h-4/5">
-      <h2 className="mb-8 justify-self-center self-end font-heading text-3xl font-medium tablet:text-4xl">
+    <div className="grid grid-cols-1 grid-rows-mobile text-white h-full tablet:h-4/5 web:grid-cols-[minmax(auto,_1fr)_20px_minmax(auto,_1fr)] web:h-full web:mb-8 web:gap-x-8">
+      <h2 className="mb-8 justify-self-center self-end font-heading text-3xl font-medium tablet:text-4xl web:col-span-3 web:row-start-1">
         Werkervaring
       </h2>
-      <div className="flex flex-row">
-        <button className="mt-2 mr-2 h-fit" onClick={() => showingInfo(1)}>
+      <div className={`${isDesktop ? 'col-start-2 row-start-2 row-span-4 flex h-full w-0.5 justify-self-center rounded bg-accent' : 'hidden'}`}></div>
+      <div className="flex flex-row web:col-start-1 web:row-start-2 web:justify-end">
+        <button className='mt-2 mr-2 h-fit web:hidden' onClick={() => showingInfo(1)}>
           <Play className={`fill-accent stroke-none ${showInfo === 1 && !isTablet ? 'origin-center rotate-90' : 'origin-center rotate-0'}`} size={32} />
         </button>
-        <div className="flex flex-col items-start">
-          <div className="flex flex-col items-start">
+        <div className="flex flex-col items-start web:items-end">
+          <div className="flex flex-col items-start web:items-end">
             <h3 className="text-left font-heading text-2xl font-medium">
               Graphic Designer - Stage
             </h3>
@@ -144,7 +146,7 @@ export default () => {
               van 2020-03-02 tot 2020-03-21 (corona)
             </p>
           </div>
-          <div className={`flex-col items-start ${showInfo === 1 || isTablet ? 'flex' : 'hidden'} `}>
+          <div className={`flex-col items-start ${showInfo === 1 || isTablet ? 'flex' : 'hidden'} web:items-end`}>
             <p className="font-body ">
               Logo's ontwerpen, social media video's maken, enz.
             </p>
@@ -159,8 +161,8 @@ export default () => {
           </div>
         </div>
       </div>
-      <div className="flex flex-row">
-        <button className="mt-2 mr-2 h-fit" onClick={() => showingInfo(2)}>
+      <div className="flex flex-row web:col-start-3 web:row-start-3">
+        <button className='mt-2 mr-2 h-fit web:hidden' onClick={() => showingInfo(2)}>
           <Play className={`fill-accent stroke-none ${showInfo === 2 && !isTablet ? 'origin-center rotate-90' : 'origin-center rotate-0'}`} size={32} />
         </button>
         <div className="flex flex-col items-start">
@@ -188,12 +190,12 @@ export default () => {
           </div>
         </div>
       </div>
-      <div className="flex flex-row">
-        <button className="mt-2 mr-2 h-fit" onClick={() => showingInfo(3)}>
+      <div className="flex flex-row web:col-start-1 web:row-start-4 web:justify-end">
+        <button className='mt-2 mr-2 h-fit web:hidden' onClick={() => showingInfo(3)}>
           <Play className={`fill-accent stroke-none ${showInfo === 3 && !isTablet ? 'origin-center rotate-90' : 'origin-center rotate-0'}`} size={32} />
         </button>
-        <div className="flex flex-col items-start">
-          <div className="flex flex-col items-start">
+        <div className="flex flex-col items-start web:items-end">
+          <div className="flex flex-col items-start web:items-end">
           <h3 className="text-left font-heading text-2xl font-medium">
           Onderhoud - Vakantiejob
         </h3>
@@ -201,7 +203,7 @@ export default () => {
           Zomer 2019, 2020, 2021 en 2022
         </p>
           </div>
-          <div className={`flex-col items-start ${showInfo === 3 || isTablet ? 'flex' : 'hidden'}`}>
+          <div className={`flex-col items-start ${showInfo === 3 || isTablet ? 'flex' : 'hidden'} web:items-end`}>
           <p className="font-body">
           Het poetsen van kamers, gangen, ramen, leefruimtes, enz.
         </p>
@@ -216,8 +218,8 @@ export default () => {
           </div>
         </div>
       </div>
-      <div className="flex flex-row">
-        <button className="mt-2 mr-2 h-fit" onClick={() => showingInfo(4)}>
+      <div className="flex flex-row web:col-start-3 web:row-start-5">
+        <button className='mt-2 mr-2 h-fit web:hidden' onClick={() => showingInfo(4)}>
           <Play className={`fill-accent stroke-none ${showInfo === 4 && !isTablet ? 'origin-center rotate-90' : 'origin-center rotate-0'}`} size={32} />
         </button>
         <div className="flex flex-col items-start">
