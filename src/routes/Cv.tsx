4abc948cbe413e3c99944e8fd613c549dd2f862e
KeyngnaertLotte <lotte.keyngnaert@student.howest.be>
2023-05-01 12:17:03 +0200

    feat: download cv name change

--- src/routes/Cv.tsx
+++ src/routes/Cv.tsx
@@ -57,7 +57,7 @@ export default function Cv() {
        <button className={`w-24 h-1 bg-white rounded focus:ring focus:ring-accent focus:rounded focus:outline-none focus:opacity-100 ${active === 0 ? `opacity-1` : 'opacity-50'}`} onClick={() => {setActive(0), setPauseTimer(true)}}/>
        <button className={`w-24 h-1 bg-white rounded focus:ring focus:ring-accent focus:rounded focus:outline-none focus:opacity-100 ${active === 1 ? `opacity-1` : 'opacity-50'}`} onClick={() => {setActive(1), setPauseTimer(true)}}/>
        <button className={`w-24 h-1 bg-white rounded focus:ring focus:ring-accent focus:rounded focus:outline-none focus:opacity-100 ${active === 2 ? `opacity-1` : 'opacity-50'}`} onClick={() => {setActive(2), setPauseTimer(true)}}/>
-        <a href={cvLotte} download={cvLotte} className='absolute hidden tablet:block tablet:right-[10%] web:right-[15%] text-white focus:ring-2 focus:ring-accent focus:rounded focus:outline-none'>Download cv</a>
+        <a href={cvLotte} download="CV_LotteKeyngnaert" className='absolute hidden tablet:block tablet:right-[10%] web:right-[15%] text-white focus:ring-2 focus:ring-accent focus:rounded focus:outline-none'>Download cv</a>
      </div>
       </div>
     </main>
