67f8c9c35b23d897f07116dd263521282c24b921
KeyngnaertLotte <lotte.keyngnaert@student.howest.be>
2024-02-13 20:08:20 +0100

    small color change

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,43 +3,30 @@ module.exports = {
   content: ['./index.html', './src/**/*.{ts,tsx}'],
   theme: {
     fontFamily: {
-      heading: [
-        'gravesend-sans',
-        'Arial',
-        'Helvetica',
-        'sans-serif',
-      ],
-      body: [
-        'segoe ui',
-        'sans-serif',
-        'Arial',
-        'Helvetica',
-      ],
-      special: [
-        "highest-praise", 
-        'sans-serif'
-      ],
+      heading: ['gravesend-sans', 'Arial', 'Helvetica', 'sans-serif'],
+      body: ['segoe ui', 'sans-serif', 'Arial', 'Helvetica'],
+      special: ['highest-praise', 'sans-serif'],
     },
     screens: {
-      'web': '840px',
-      'tablet': '560px',
+      web: '840px',
+      tablet: '560px',
     },
     extend: {
       colors: {
-        accent: '#d4af37',
+        accent: '#B27F00',
       },
       gridTemplateRows: {
-        'layout': 'repeat(2, minmax(0, 0.4fr))',
-        'tablet': '100px minmax(0, 0.3fr) minmax(0, 0.3fr)',
-        'mobile': '100px repeat(4,minmax(0, auto))',
-        'web': '100px minmax(0, 0.3fr) minmax(0, 0.3fr)',
+        layout: 'repeat(2, minmax(0, 0.4fr))',
+        tablet: '100px minmax(0, 0.3fr) minmax(0, 0.3fr)',
+        mobile: '100px repeat(4,minmax(0, auto))',
+        web: '100px minmax(0, 0.3fr) minmax(0, 0.3fr)',
       },
       blur: {
-        'xs' : '2px',
+        xs: '2px',
       },
       brightness: {
         25: '.25',
-      }
+      },
     },
   },
   plugins: [],
