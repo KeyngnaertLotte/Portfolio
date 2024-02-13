8365135e8a391f6e566abef3858dc03551284428
KeyngnaertLotte <lotte.keyngnaert@student.howest.be>
2024-02-13 23:40:19 +0100

    eh

--- src/components/CV/AppOpleiding.tsx
+++ src/components/CV/AppOpleiding.tsx
@@ -4,14 +4,42 @@ import HorizontalLine from './HorizontalLine';
 
 export default () => {
   return (
-    <div className='h-full w-full flex items-center justify-center pt-4'>
-        
-      <HorizontalLine/>
-      <div>
-        <div>
-          <h2 className="text-white font-heading text-3xl font-medium tablet:text-2xl">Athena Campus Heule</h2>
-          <p className="text-white font-body text-lg tablet:text-base">2014-2018</p>
-        </div>
+    <div className='h-full w-full flex items-center justify-center pt-4 text-white font-body'>
+      <div className='w-[70%] grid grid-rows-5 grid-cols-7 grid-cols-10px grid-rows-10px ml-4 justify-items-center content-end gap-0 gap-y-4'> 
+      <div className="col-start-1 col-span-1 row-span-5 row-start-1   w-2 rounded-full bg-[#B27F00]"></div>
+      <div className=" h-4 w-4 rounded-full bg-[#B27F00]  col-span-1 col-start-1 row-span-1 row-start-2"></div>
+        <div className="h-4 w-4 rounded-full bg-[#B27F00] col-span-1 col-start-1 row-span-1 row-start-3"></div>
+        <div className="h-4 w-4 rounded-full bg-[#B27F00] col-span-1 col-start-1 row-span-1 row-start-4"></div>
+        <div className="h-4 w-4 rounded-full bg-[#B27F00] col-span-1 col-start-1 row-span-1 row-start-5"></div>
+
+          <p className='font-bold row-start-2 row-span-1 col-start-2 col-span-2'>2020 - heden</p>
+          <div className='row-start-2 row-span-1 col-start-4 col-span-4 flex flex-col justify-self-start'>
+            <p className=''>MCT - Multimedia Creative Technologie</p>
+            <p className=''>Howest, Kortrijk</p>
+            <p className='text-zinc-500'>Afstudeer richting: Next Web Developer</p>
+          </div>
+          
+          <p className='font-bold row-start-3 row-span-1 col-start-2 col-span-2'>2018 - 2020</p>
+          <div className='row-start-3 row-span-1 col-start-4 col-span-4 flex flex-col justify-self-start'>
+            <p className=''>Multimedia Visueel</p>
+            <p className=''>Athena Heule, Heule</p>
+            <p className='text-zinc-500'>Designen en creeëren in Adobe apps en basis websites bouwen</p>
+          </div>
+
+          <p className='font-bold row-start-4 row-span-1 col-start-2 col-span-2'>2016 - 2018</p>
+          <div className='row-start-4 row-span-1 col-start-4 col-span-4 flex flex-col justify-self-start'>
+            <p className=''>Handel & IT</p>
+            <p className=''>Heilige Familie, Ieper</p>
+            <p className='text-zinc-500'>Basis boekhouden, basis Office pakket en basis opbouw computer</p>
+          </div>
+
+          <p className='font-bold row-start-5 row-span-1 col-start-2 col-span-2'>2014 - 2016</p>
+          <div className='row-start-5 row-span-1 col-start-4 col-span-4 flex flex-col justify-self-start'>
+            <p className=''>Moderne</p>
+            <p className=''>Lyceum, Ieper</p>
+            <p className='text-zinc-500'>Basis vakken</p>
+          </div>
+          
       </div>
     </div>
   );
--- src/components/CV/HorizontalLine.tsx
+++ src/components/CV/HorizontalLine.tsx
@@ -1,25 +1,13 @@
 export default () => {
-      return (
-        //     <svg className='h-full w-full' width={100} height={100}>
-    // <line className="fill-none stroke-[#b17f11] rounded-full stroke-2" height={100} x1="2.38" y1="2.38" x2="2.38" y2="331.72"/>
-    // <rect className="fill-[#b17f11] stroke-0" x="0" y="0" width="4.77" height="4.77" rx="2.38" ry="2.38"/>
-    // <rect className="fill-[#b17f11] stroke-0" x="0" y="329.34" width="4.77" height="4.77" rx="2.38" ry="2.38"/>
-    // <rect className="fill-[#b17f11] stroke-0" x="0" y="82.33" width="4.77" height="4.77" rx="2.38" ry="2.38"/>
-    // <rect className="fill-[#b17f11] stroke-0" x="0" y="247" width="4.77" height="4.77" rx="2.38" ry="2.38"/>
-    // <rect className="fill-[#b17f11] stroke-0" x="0" y="164.67" width="4.77" height="4.77" rx="2.38" ry="2.38"/>
-    // </svg>
-    <div className="relative h-full flex flex-row justify-center items-center">
-    <div className="w-2  h-full bg-yellow-400 rounded-full flex-grow-1"></div>
-    <div className="absolute flex flex-col h-full justify-between">
-      {/* Content of the second sibling div */}
-      <div className="w-4 h-4 bg-yellow-400 rounded-full mt-3"></div>
-      <div className="w-4 h-4 bg-yellow-400 rounded-full"></div>
-      <div className="w-4 h-4 bg-yellow-400 rounded-full"></div>
-      <div className="w-4 h-4 bg-yellow-400 rounded-full"></div>
+  return (
+    <div className="relative flex h-full w-auto flex-row items-center justify-center">
+      <div className="flex-grow-1  h-full w-2 rounded-full bg-[#B27F00]"></div>
+      <div className="absolute flex h-full flex-col justify-between">
+        <div className=" h-4 w-4 rounded-full bg-[#B27F00] mt-3"></div>
+        <div className="h-4 w-4 rounded-full bg-[#B27F00]"></div>
+        <div className="h-4 w-4 rounded-full bg-[#B27F00]"></div>
+        <div className="h-4 w-4 rounded-full bg-[#B27F00]"></div>
+      </div>
     </div>
-  </div>
-  
-
-
-      )
-    }
+  )
+}
