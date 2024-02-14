16c63e73f3e61ff31e36d743b592b884c01e36f0
KeyngnaertLotte <lotte.keyngnaert@student.howest.be>
2024-02-14 16:27:27 +0100

    nav cv ok

--- src/components/CV/AppWerkervaring.tsx
+++ src/components/CV/AppWerkervaring.tsx
@@ -1,110 +1,110 @@
 import { Link } from 'lucide-react'
-import athenaHeule from '../../assets/img/AthenaCampusHeule.jpg'
-import HorizontalLine from './HorizontalLine'
-import { useState } from 'react';
+import { useState } from 'react'
 
 export default () => {
-  const [selectedYear, setSelectedYear] = useState(2023);
+  const [selectedYear, setSelectedYear] = useState(2023)
 
   const handleRadioChange = (value: any, textColor: any) => {
-    setSelectedYear(value);
-    console.log('Selected Year:', value);
-    console.log('Text Color:', textColor);
-  };
+    setSelectedYear(value)
+    console.log('Selected Year:', value)
+    console.log('Text Color:', textColor)
+  }
 
   const jobs = [
     {
       year: [2023],
       job: 'Administratief bediende - studentenjob',
       company: 'Cid Lines, Ieper',
-      description: 'Enkele weken in de zomer. Ophalen van gevraagde info van producten uit een Excel/server/database en plaatsen in een overzichtelijke Excel.'
+      description:
+        'Enkele weken in de zomer. Ophalen van gevraagde info van producten uit een Excel/server/database en plaatsen in een overzichtelijke Excel.',
     },
     {
       year: [2023],
       job: 'Onderhoud - studentenjob',
       company: 'Gemeente Zonnebeke, Zonnebeke',
-      description: 'Enkele weken in de zomer. Het poetsen van OC zalen, leefkeukens, bureau\'s, enz.'
+      description:
+        "Enkele weken in de zomer. Het poetsen van OC zalen, leefkeukens, bureau's, enz.",
     },
     {
       year: [2018, 2019, 2023],
       job: 'Administratief bediende - studentenjob',
       company: 'Vandelanotte, Kortrijk',
-      description: 'Enkele weken in de zomer. Inscannen van documenten en plaatsen in de correcte map, en nieuwe klanten in het portaal plaatsen.'
+      description:
+        'Enkele weken in de zomer. Inscannen van documenten en plaatsen in de correcte map, en nieuwe klanten in het portaal plaatsen.',
     },
     {
       year: [2020, 2021, 2022],
       job: 'Onderhoud - studentenjob',
       company: 'Huize Zonnelied, Ieper',
-      description: 'Enkele weken in de zomer. Het poetsen van kamers, gangen, ramen, leefruimtes, enz.'
+      description:
+        'Enkele weken in de zomer. Het poetsen van kamers, gangen, ramen, leefruimtes, enz.',
     },
     {
       year: 2020,
       job: 'Graphic Designer - stage',
       company: 'Impressionant, Ieper',
-      description: '2 weken in maart. Vroeg gestopt door corona. Logo\'s ontwerpen, video\'s maken in After Effects, enz.'
+      description:
+        "2 weken in maart. Vroeg gestopt door corona. Logo's ontwerpen, video's maken in After Effects, enz.",
     },
     {
       year: '2018 - heden',
       job: 'Oppassen',
       company: 'Diversen, Westhoek',
-      description: 'Leeftijden van 1 jaar tot 12 jaar.'
-    }
+      description: 'Leeftijden van 1 jaar tot 12 jaar.',
+    },
   ]
-
-  
 
   return (
     <div className="flex h-full w-full items-center justify-center pt-4 font-body text-white">
-      {/* 
-        <div className='flex flex-col row-span-1 row-start-1 col-start-3 col-span-full'>
-          <p>Administratief bediende - studentenjob</p>
-          <p>Cid Lines, Ieper</p>
-          <p className='text-sm text-zinc-500'>Enkele weken in de zomer. Ophalen van gevraagde info van producten uit een Excel/server/database en plaatsen in een overzichtelijke Excel.</p>
+      <div className="grid-rows-10 grid h-full w-[20%]  justify-items-start">
+        <div className="col-span-1 col-start-1 row-start-1 row-end-9 w-2 justify-self-center rounded-full bg-[#B27F00]"></div>
+        <div className="col-span-1 col-start-1 row-span-1 row-start-2 h-4 w-4 justify-self-center rounded-full bg-[#B27F00]"></div>
+        <div className="col-span-1 col-start-1 row-span-1 row-start-3  h-4 w-4 justify-self-center rounded-full bg-[#B27F00]"></div>
+        <div className="col-span-1 col-start-1 row-span-1 row-start-4 h-4 w-4 justify-self-center rounded-full bg-[#B27F00]"></div>
+        <div className="col-span-1 col-start-1 row-span-1 row-start-5 h-4 w-4 justify-self-center rounded-full bg-[#B27F00]"></div>
+        <div className="col-span-1 col-start-1 row-span-1 row-start-6 h-4 w-4 justify-self-center rounded-full bg-[#B27F00]"></div>
+        <div className="col-span-1 col-start-1 row-span-1 row-start-7 h-4 w-4 justify-self-center rounded-full bg-[#B27F00]"></div>
+        <div className="col-span-1 col-start-1 row-span-1 row-start-8 h-4 w-4 justify-self-center rounded-full bg-[#B27F00]"></div>
+
+        {[2023, 2022, 2021, 2020, 2019, 2018, '2018 - heden'].map(
+          (year, index) => (
+            <label
+              key={index}
+              className={`row-span-1 cursor-pointer font-bold row-start-${
+                index + 2
+              } col-span-1 col-start-2 self-start ${
+                selectedYear === year ? 'text-[#B27F00]' : ''
+              }`}
+            >
+              <input
+                type="radio"
+                value={year}
+                className="sr-only"
+                onChange={() => handleRadioChange(year, '#B27F00')}
+                checked={selectedYear === year}
+              />
+              <span className="">{year}</span>
+            </label>
+          ),
+        )}
+      </div>
+
+      <div className="h-full w-[40%]">
+        <div className="flex h-full w-full flex-col items-start justify-center gap-6">
+          {jobs.map(
+            (job, index) =>
+              (Array.isArray(job.year)
+                ? job.year.includes(selectedYear)
+                : job.year === selectedYear) && (
+                <div key={index} className="mb-4">
+                  <p className="font-bold">{job.job}</p>
+                  <p>{job.company}</p>
+                  <p className="text-sm text-zinc-500">{job.description}</p>
+                </div>
+              ),
+          )}
         </div>
-       </div> */}
-       <div className='grid h-full w-[20%] grid-rows-10  justify-items-start'>
-       <div className="col-start-1 col-span-1 row-start-1 row-end-9 w-2 rounded-full bg-[#B27F00] justify-self-center"></div>
-        <div className="col-span-1 col-start-1 row-span-1 row-start-2 h-4 w-4 rounded-full bg-[#B27F00] justify-self-center"></div>
-        <div className="col-span-1 col-start-1 row-span-1 row-start-3  h-4 w-4 rounded-full bg-[#B27F00] justify-self-center"></div>
-        <div className="col-span-1 col-start-1 row-span-1 row-start-4 h-4 w-4 rounded-full bg-[#B27F00] justify-self-center"></div>
-        <div className="col-span-1 col-start-1 row-span-1 row-start-5 h-4 w-4 rounded-full bg-[#B27F00] justify-self-center"></div>
-        <div className="col-span-1 col-start-1 row-span-1 row-start-6 h-4 w-4 rounded-full bg-[#B27F00] justify-self-center"></div>
-        <div className="row-start-7 col-span-1 col-start-1 row-span-1 h-4 w-4 rounded-full bg-[#B27F00] justify-self-center"></div>
-        <div className="row-start-8 col-span-1 col-start-1 row-span-1 h-4 w-4 rounded-full bg-[#B27F00] justify-self-center"></div>
-
-        {[2023, 2022, 2021, 2020, 2019, 2018, '2018 - heden'].map((year, index) => (
-        <label
-          key={index}
-          className={`cursor-pointer font-bold row-span-1 row-start-${index + 2} col-start-2 col-span-1 self-start ${
-            selectedYear === year ? 'text-[#B27F00]' : ''
-          }`}
-        >
-          <input
-            type="radio"
-            value={year}
-            className="sr-only"
-            onChange={() => handleRadioChange(year, '#B27F00')}
-            checked={selectedYear === year}
-          />
-          <span className="">{year}</span>
-        </label>
-      ))}
-       </div>
-
-       <div className='w-[40%] h-full'>
-  <div className='w-full h-full flex flex-col items-start justify-center gap-6'>
-    {jobs.map((job, index) => (
-      (Array.isArray(job.year) ? job.year.includes(selectedYear) : job.year === selectedYear) && (
-        <div key={index} className="mb-4">
-          <p className="font-bold">{job.job}</p>
-          <p>{job.company}</p>
-          <p className='text-sm'>{job.description}</p>
-        </div>
-      )
-    ))}
-  </div>
-</div>
-
+      </div>
     </div>
   )
 }
--- src/routes/Cv.tsx
+++ src/routes/Cv.tsx
@@ -1,37 +1,30 @@
-import { useEffect, useState } from 'react'
-import AppNavigation from '../components/AppNavigation'
-import AppComputervaardigheden from '../components/CV/AppComputervaardigheden'
-import AppOpleiding from '../components/CV/AppOpleiding'
-import AppWerkervaring from '../components/CV/AppWerkervaring'
-import { useMediaQuery } from 'react-responsive'
-import { Menu, X } from 'lucide-react'
-import cvLotte from '../assets/files/CV_LotteKeyngnaert.pdf'
+import { useEffect, useState } from 'react';
+import AppNavigation from '../components/AppNavigation';
+import AppComputervaardigheden from '../components/CV/AppComputervaardigheden';
+import AppOpleiding from '../components/CV/AppOpleiding';
+import AppWerkervaring from '../components/CV/AppWerkervaring';
+import { useMediaQuery } from 'react-responsive';
+import { Menu, X } from 'lucide-react';
+import cvLotte from '../assets/files/CV_LotteKeyngnaert.pdf';
 
 export default function Cv() {
-  const [isChecked, setIsChecked] = useState(false)
-
-  const handleInputChange = () => {
-    setIsChecked(!isChecked)
-  }
-
-  const isMobile = useMediaQuery({ query: '(max-width: 840px)' })
-  const [showNav, setShowNav] = useState(false)
+  const isMobile = useMediaQuery({ query: '(max-width: 840px)' });
+  const [showNav, setShowNav] = useState(false);
+  const [selectedTab, setSelectedTab] = useState('opleiding'); // Initial selected tab
 
   const showNavigation = () => {
-    if (showNav) {
-      setShowNav(false)
-    } else {
-      setShowNav(true)
-    }
-  }
+    setShowNav(!showNav); // Toggle nav state
+  };
+
+  const handleRadioChange = (event: any) => {
+    setSelectedTab(event.target.value); // Update selected tab on radio change
+  };
 
   return (
     <main className="flex h-screen w-screen flex-row">
       <div
         className={`z-20 flex flex-col overflow-hidden bg-black ${
-          showNav
-            ? 'absolute top-0 left-0 h-screen w-screen translate-x-0  p-4'
-            : 'none h-0 w-0 translate-x-full  p-0'
+          showNav ? 'absolute top-0 left-0 h-screen w-screen translate-x-0 p-4' : 'none h-0 w-0 translate-x-full p-0'
         }`}
       >
         <button className="self-end" onClick={showNavigation}>
@@ -41,22 +34,18 @@ export default function Cv() {
         <a
           href={cvLotte}
           download={cvLotte}
-          className="absolute bottom-[15%]  block text-lg text-white tablet:hidden"
+          className="absolute bottom-[15%] block text-lg text-white tablet:hidden"
         >
           Download cv
         </a>
       </div>
       <div className="flex h-screen w-full flex-col items-center p-4 pb-0 ">
-        <div className="flex w-full flex-row justify-between  web:w-9/12 ">
+        <div className="flex w-full flex-row justify-between web:w-9/12 ">
           <h1 className="font-heading text-4xl font-bold text-white">
             Lotte Keyngnaert
           </h1>
           {isMobile ? (
-            <button
-              className="self-start"
-              tabIndex={-1}
-              onClick={showNavigation}
-            >
+            <button className="self-start" tabIndex={-1} onClick={showNavigation}>
               <Menu className="stroke-white" size={60} />
             </button>
           ) : (
@@ -66,39 +55,39 @@ export default function Cv() {
         <div className="flex h-[85%] w-full flex-row">
           <AppComputervaardigheden />
           <div className="mt-10 w-4/5">
-            <div className="flex w-full flex-row items-center justify-center gap-4">
-              <label className="cursor-pointer">
+            <div className="flex w-fit mx-auto rounded flex-row items-center justify-center bg-zinc-900 divide-x-2 divide-zinc-700">
+              <label className="cursor-pointer px-3">
                 <input
                   type="radio"
-                  value="some-value"
-                  checked={isChecked}
-                  onChange={handleInputChange}
+                  value="opleiding"
+                  checked={selectedTab === 'opleiding'} 
+                  onChange={handleRadioChange}
                   className="sr-only"
                 />
-                <span className="font-heading text-3xl font-medium text-white tablet:text-2xl">
+                <span className={`font-heading text-3xl font-medium  tablet:text-2xl ${selectedTab === 'opleiding' ? 'text-white' : "text-zinc-500"}`}>
                   Opleiding
                 </span>
               </label>
-              <label className="cursor-pointer">
+              <label className="cursor-pointer px-3">
                 <input
                   type="radio"
-                  value="some-value"
-                  checked={isChecked}
-                  onChange={handleInputChange}
+                  value="werkervaring"
+                  checked={selectedTab === 'werkervaring'} 
+                  onChange={handleRadioChange}
                   className="sr-only"
                 />
-                <span className="font-heading text-3xl font-medium text-white tablet:text-2xl">
+                <span className={`font-heading text-3xl font-medium  tablet:text-2xl ${selectedTab === 'werkervaring' ? 'text-white' : "text-zinc-500"}`} >
                   Werkervaring
                 </span>
               </label>
             </div>
             <div className="h-full">
-              {/* <AppOpleiding /> */}
-              <AppWerkervaring />
+              {selectedTab === 'opleiding' && <AppOpleiding />}
+              {selectedTab === 'werkervaring' && <AppWerkervaring />}
             </div>
           </div>
         </div>
       </div>
     </main>
-  )
+  );
 }
